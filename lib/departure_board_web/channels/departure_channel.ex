803ef76e58b177d59d9bff4f340458cc8d7c3a0e
Rebecca Paz <r.pazyaquian@gmail.com>
2018-01-15 13:41:12 -0500

    Add callback for publishing data

--- lib/departure_board_web/channels/departure_channel.ex
+++ lib/departure_board_web/channels/departure_channel.ex
@@ -1,7 +1,15 @@
-defmodule HelloWeb.DepartureChannel do
+defmodule DepartureBoardWeb.DepartureChannel do
   use Phoenix.Channel
 
   def join("departure:data", _message, socket) do
     {:ok, socket}
+  end
+
+  def update_data(data) do
+    DepartureBoardWeb.Endpoint.broadcast(
+      "departure:data",
+      "update",
+      %{data: data}
+    )
   end
 end
