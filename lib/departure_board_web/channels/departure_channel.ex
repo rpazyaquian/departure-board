965e47a1cd1e7e51ca039a23d7ac69a3a7295344
Rebecca Paz <r.pazyaquian@gmail.com>
2018-01-15 14:26:03 -0500

    Send data when someone joins

--- lib/departure_board_web/channels/departure_channel.ex
+++ lib/departure_board_web/channels/departure_channel.ex
@@ -2,6 +2,10 @@ defmodule DepartureBoardWeb.DepartureChannel do
   use Phoenix.Channel
 
   def join("departure:data", _message, socket) do
+    # TODO: figure out how to do this in a more robust way
+    # this needs to finish in order to completely open the socket
+    # but i also want to call DepartureBoard.Fetcher.get_data as part of this
+    Process.send_after(DepartureBoard.Fetcher, :get_data, :timer.seconds(3))
     {:ok, socket}
   end
 
